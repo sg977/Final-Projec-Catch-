267404a6ea5b989cfe0a9dc083e54ab182bbe104
Siyi Guo <guosiyi1993@gmail.com>
2018-10-16 22:54:15 -0400

    coco first draft

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import DateCard from "./pages/DateCard";
-import InstantMessage from "./pages/InstantMessage";
+
+import Landing from "./pages/Landing";
+import SignUp from "./pages/SignUp"; 
+
+import Login from "./pages/Login";
+// import First from "./pages/SignUp";
 
 const App = () => (
   <Router>
     <div>
       <Switch>
-        <Route exact path="/" component={DateCard} />
-        <Route exact path="/dates" component={DateCard} />
-        <Route exact path="/message" component={InstantMessage} />
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/Login"  component={Login} />
+        <Route exact path="/Signup" component={SignUp} />
+        {/* <Route exact path="/dates"  />
+        <Route exact path="/message" component={InstantMessage} /> */}
       </Switch>
     </div>
   </Router>
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export default {
   // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  landing: function() {
+    return axios.get("/api/");
   },
   // Gets the book with the given id
   getBook: function(id) {
