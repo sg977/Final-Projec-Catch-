ad1e3ff51ec3edaed362b2774063118338c0b5bd
pjmccall <pjmccall13@gmail.com>
2018-10-20 08:37:43 -0400

    adding routes for user

--- models/user.js
+++ models/user.js
@@ -3,9 +3,16 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   email: { type: String, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  age: { type: Number, required: true },
+  neighborhood: { type: String, required: true },
+  gender: { type: String, required: true },
+  genderInterest: { type: String, required: true },
+  date: { type: Date, default: Date.now }
 });
 
-const User = mongoose.model("User", bookSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
