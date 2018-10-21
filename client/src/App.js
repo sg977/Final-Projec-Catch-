38b7a847df218aa9d94e6e54805c686ffb868e40
pjmccall <pjmccall13@gmail.com>
2018-10-21 10:44:56 -0400

    activated signup and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import DateCard from "./pages/DateCard";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 const App = () => (
   <Router>
     <div>
       <Switch>
-        <Route exact path="/" component={DateCard} />
+        <Route exact path="/" component={SignUp} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
         <Route exact path="/dates" component={DateCard} />
       </Switch>
     </div>
